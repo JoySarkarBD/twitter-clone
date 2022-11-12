3158d774695d17ff6e9bdaf0d9f069feaf52b4e7
JoySarkarBD <joysarkarbd407@gmail.com>
2022-11-12 19:20:47 +0600

    tweet img file to dom and remove function added

--- public/js/home.js
+++ public/js/home.js
@@ -1,8 +1,12 @@
+// dom reference
 const user_info = document.getElementById("user_info");
 const main = document.getElementById("main");
 const logout_add_account =
   document.getElementsByClassName("logout_add_account");
+const tweetContentTextArea = document.querySelector("textarea#tweetContent");
+const tweetBtn = document.querySelector("button.createTweetBtn");
 
+// logout modal
 user_info.addEventListener("click", function () {
   if (logout_add_account[0].classList[2] === "invisible") {
     logout_add_account[0].classList.remove("invisible");
@@ -11,8 +15,8 @@ user_info.addEventListener("click", function () {
   }
 });
 
-const tweetContentTextArea = document.querySelector("textarea#tweetContent");
-const tweetBtn = document.querySelector("button.createTweetBtn");
+// tweet text area
+
 tweetContentTextArea.addEventListener("input", function () {
   const val = this.value;
   if (val) {
@@ -21,5 +25,67 @@ tweetContentTextArea.addEventListener("input", function () {
   } else {
     tweetBtn.setAttribute("disabled", true);
     tweetBtn.style.backgroundColor = "#8ecdf8";
+  }
+});
+
+//img preview for user to tweet
+
+// dom reference
+const tweetImg = document.querySelector("input#postImage");
+const tweetedImgContainer = document.querySelector(".img_container");
+const tweetedImgs = [];
+
+tweetImg.addEventListener("change", function (e) {
+  const imgFile = this.files;
+
+  [...imgFile].forEach((file) => {
+    if (
+      ["image/jpg", "image/jpeg", "image/png", "image/svg+xml"].includes(
+        file.type
+      )
+    ) {
+      tweetBtn.removeAttribute("disabled");
+      tweetBtn.style.backgroundColor = "rgb(29, 155, 240)";
+      tweetedImgs.push(file);
+      const fr = new FileReader();
+      fr.onload = function () {
+        const htmlEle = document.createElement("div");
+        htmlEle.classList.add("img");
+        htmlEle.dataset.name = file.name;
+        htmlEle.innerHTML = `<span id="cross_btn">
+                              <i class="fas fa-times"></i>
+                             </span><img>`;
+        const img = htmlEle.querySelector("img");
+        img.src = fr.result;
+        tweetedImgContainer.appendChild(htmlEle);
+      };
+      fr.readAsDataURL(file);
+    } else {
+      return;
+    }
+  });
+});
+
+// remove img from tweeted image container
+tweetedImgContainer.addEventListener("click", function (e) {
+  const cross_btn = e.target.id === "cross_btn" ? e.target : null;
+
+  if (cross_btn) {
+    const imgEle = cross_btn.parentElement;
+    const fileName = imgEle.dataset.name;
+
+    tweetedImgs.forEach((img, i) => {
+      if (fileName === img.name) {
+        tweetedImgs.splice(i, 1);
+        imgEle.remove();
+
+        if (!tweetedImgs.length && !tweetContentTextArea.value.trim()) {
+          tweetBtn.setAttribute("disabled", true);
+          tweetBtn.style.backgroundColor = "#8ecdf8";
+        }
+      }
+    });
+  } else {
+    return;
   }
 });
